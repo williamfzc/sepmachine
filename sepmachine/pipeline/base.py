54cbfa13379ca3524c0465259ce4aa8f789b7505
williamfzc <178894043@qq.com>
2020-02-26 15:58:53 +0800

    feat(pipeline): loop_run and error_ignore

--- sepmachine/pipeline/base.py
+++ sepmachine/pipeline/base.py
@@ -1,6 +1,7 @@
 from sepmachine.capture.base import BaseCapture
 from sepmachine.handler.base import BaseHandler
 
+import typing
 from loguru import logger
 
 
@@ -12,19 +13,32 @@ class BasePipeline(object):
         logger.info(f"capture: {self.capture.__class__}")
         logger.info(f"handler: {self.handler.__class__}")
 
-    def run(self, video_path: str):
-        logger.info("start pipeline")
-        logger.info(f"video: {video_path}")
+    def run(self, video_path: str) -> bool:
+        try:
+            logger.info("start pipeline")
+            logger.info(f"video: {video_path}")
 
-        # capture
-        self.capture.start(video_path)
-        logger.info("start recording")
-        self.capture.operate()
-        logger.info("stop recording")
-        capture_result: bool = self.capture.end()
-        assert capture_result, "capture error"
+            # capture
+            self.capture.start(video_path)
+            logger.info("start recording")
+            self.capture.operate()
+            logger.info("stop recording")
+            capture_result: bool = self.capture.end()
+            assert capture_result, "capture error"
 
-        # handler
-        handle_result: bool = self.handler.handle(video_path)
-        assert handle_result, "handler error"
-        logger.info("end pipeline")
+            # handler
+            handle_result: bool = self.handler.handle(video_path)
+            assert handle_result, "handler error"
+            logger.info("end pipeline")
+            return True
+        except Exception as e:
+            logger.error(e)
+            return False
+
+    def loop_run(self, video_path: str, loop_num: int) -> typing.List[bool]:
+        ret_list = list()
+        for index in range(loop_num):
+            logger.info(f"loop: {index}")
+            ret = self.run(video_path)
+            ret_list.append(ret)
+        return ret_list
