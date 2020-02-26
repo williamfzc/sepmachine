b8d8ac90aecc65cd89a1e0ef1a7663ccd6819147
williamfzc <178894043@qq.com>
2020-02-26 15:59:19 +0800

    chore(example): new format

--- example/android/custom.py
+++ example/android/custom.py
@@ -12,9 +12,9 @@ WEIXIN_PACKAGE_NAME = "com.tencent.mm"
 
 # custom capture
 class MyAdbCapture(AdbCapture):
-    def __init__(self, *args, **kwargs):
-        super(MyAdbCapture, self).__init__(*args, **kwargs)
+    def start(self, video_path: str) -> bool:
         self.prepare()
+        return super(MyAdbCapture, self).start(video_path)
 
     def prepare(self):
         self.u2_device = u2.connect(self.serial_no)
@@ -33,19 +33,30 @@ class MyAdbCapture(AdbCapture):
 
 # custom handler
 class MyKerasHandler(KerasHandler):
+    def __init__(self, *args, **kwargs):
+        super(MyKerasHandler, self).__init__(*args, **kwargs)
+        self.result = []
+
     def handle(self, video_path: str) -> bool:
         handler_result: bool = super(MyKerasHandler, self).handle(video_path)
         assert self.classifier_result
 
         # do some extra calculations?
         # if you want to know:
-        # how long from stage 0 end to stage 1 start?
+        # how long from stage 0 end to stage 2 start?
         end_frame_of_0 = self.classifier_result.last("0")
-        start_frame_of_1 = self.classifier_result.first("1")
-        cost = start_frame_of_1.timestamp - end_frame_of_0.timestamp
+        start_frame_of_2 = self.classifier_result.first("2")
+        cost = start_frame_of_2.timestamp - end_frame_of_0.timestamp
         print(f"stage 0 end: {end_frame_of_0.frame_id, end_frame_of_0.timestamp}")
-        print(f"stage 1 start: {start_frame_of_1.frame_id, start_frame_of_1.timestamp}")
+        print(f"stage 2 start: {start_frame_of_2.frame_id, start_frame_of_2.timestamp}")
         print(f"and the cost: {cost}")
+
+        # or maybe unstable ranges
+        unstable_ranges = self.classifier_result.get_not_stable_stage_range()
+        first_changing_frame = unstable_ranges[0][-1]
+        new_cost = start_frame_of_2.timestamp - first_changing_frame.timestamp
+        print(f"new cost: {new_cost}")
+        self.result.append([cost, new_cost])
 
         return handler_result
 
@@ -53,4 +64,5 @@ class MyKerasHandler(KerasHandler):
 adb_cap = MyAdbCapture(serial_no="9c12aa96")
 handler = MyKerasHandler(model_path="./output.h5")
 pipeline = BasePipeline(adb_cap, handler)
-pipeline.run(VIDEO_PATH)
+pipeline.loop_run(VIDEO_PATH, 3)
+print(handler.result)
