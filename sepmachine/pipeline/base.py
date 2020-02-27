0053c619d9e807b6f39602978921887b9c57703f
williamfzc <178894043@qq.com>
2020-02-27 15:40:10 +0800

    feat(pipeline): pipeline can run without video_path arg

--- sepmachine/pipeline/base.py
+++ sepmachine/pipeline/base.py
@@ -2,6 +2,8 @@ from sepmachine.capture.base import BaseCapture
 from sepmachine.handler.base import BaseHandler
 
 import typing
+import tempfile
+import os
 from loguru import logger
 
 
@@ -13,7 +15,13 @@ class BasePipeline(object):
         logger.info(f"capture: {self.capture.__class__}")
         logger.info(f"handler: {self.handler.__class__}")
 
-    def run(self, video_path: str) -> bool:
+    def run(self, video_path: str = None) -> bool:
+        use_temp: bool = False
+        if not video_path:
+            logger.info("no specific video path, use temp file")
+            use_temp = True
+            video_path = tempfile.NamedTemporaryFile(suffix=".mp4", delete=False).name
+
         try:
             logger.info("start pipeline")
             logger.info(f"video: {video_path}")
@@ -34,8 +42,16 @@ class BasePipeline(object):
         except Exception as e:
             logger.error(e)
             return False
+        finally:
+            if use_temp:
+                try:
+                    logger.debug(f"remove tempfile: {video_path}")
+                    os.remove(video_path)
+                # windows issue
+                except PermissionError as e:
+                    logger.error(e)
 
-    def loop_run(self, video_path: str, loop_num: int) -> typing.List[bool]:
+    def loop_run(self, video_path: str = None, loop_num: int = 1) -> typing.List[bool]:
         ret_list = list()
         for index in range(loop_num):
             logger.info(f"loop: {index}")
